522f6ce0cd3b9a92a69cb38f61bbd3f912c827c9
Michalis Karasavvas <karasavm@gmail.com>
2018-03-21 00:03:29 +0200

    lower case email false

--- models/User.js
+++ models/User.js
@@ -9,7 +9,7 @@ var ValidatorError  = mongoose.Error.ValidatorError;
 
 var UserSchema = new mongoose.Schema({
     //username: {type: String, lowercase: true, unique: true, required: [true, "can't be blank"], match: [/^[a-zA-Z0-9]+$/, 'is invalid'], index: true},
-    email: {type: String, lowercase: true, unique: [true, "Email is in use!"], required: [true, "Email can't be blank!"], match: [/\S+@\S+\.\S+/, 'Email is invalid!'], index: true},
+    email: {type: String, unique: [true, "Email is in use!"], required: [true, "Email can't be blank!"], match: [/\S+@\S+\.\S+/, 'Email is invalid!'], index: true},
     name: {type: String, required:[true, "Name can't be black"]},
     //image: String,
     //favorites: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Article' }],
